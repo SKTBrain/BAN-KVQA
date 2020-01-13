5ee25e79c583c97e5d0a5bac0a347623ebb1edd5
Jaesun Park <diligensloth@gmail.com>
2020-01-13 17:57:04 +0900

    Update score

--- tools/compute_softscore.py
+++ tools/compute_softscore.py
@@ -20,11 +20,11 @@ def get_score(occurences):
     if occurences == 0:
         return 0
     elif occurences == 1:
-        return 1./3
+        return .3
     elif occurences == 2:
-        return 2./3
+        return .6
     elif occurences == 3:
-        return 1.
+        return .9
     else:
         return 1.
 
